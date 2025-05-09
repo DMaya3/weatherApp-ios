32a4b1429e5c536bf1719bc66bc022975e9dd45a
David Jesus Maya Quiros <dmaya.3@outlook.com>
2025-05-09 16:03:34 +0200

    I´ve created the project

---
+++ MyWeather/MyWeather/MyWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyWeatherApp.swift
+//  MyWeather
+//
+//  Created by David Jesús Maya Quirós on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MyWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
