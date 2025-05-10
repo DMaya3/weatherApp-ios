12bf27a2e8ea159921da21aa024fa0355fe79cd4
David Jesus Maya Quiros <dmaya.3@outlook.com>
2025-05-10 16:02:07 +0200

    I´ve created Home View

---
+++ MyWeather/MyWeather/Views/Screens/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  MyWeather
+//
+//  Created by David Jesús Maya Quirós on 10/5/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    HomeView()
+}
