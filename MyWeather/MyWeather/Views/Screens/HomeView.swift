1b9731a4fef3b21d22f4da14c8c6f557289de7a8
David Jesus Maya Quiros <dmaya.3@outlook.com>
2025-05-10 16:30:16 +0200

    I´ve created a LocationManager to request location

--- MyWeather/MyWeather/Views/Screens/HomeView.swift
+++ MyWeather/MyWeather/Views/Screens/HomeView.swift
@@ -8,8 +8,18 @@
 import SwiftUI
 
 struct HomeView: View {
+    @StateObject private var locationManager = LocationManager()
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack(spacing: 20) {
+            if let location = locationManager.userLocation {
+                Text("Latitude: \(location.latitude)")
+                Text("Longitude: \(location.longitude)")
+            } else {
+                Text("Getting location...")
+            }
+        }
+        .padding()
     }
 }
 
