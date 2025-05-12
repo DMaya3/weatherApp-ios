39666db5287f83d760d973552ae68f52d3a3b209
David Jesus Maya Quiros <dmaya.3@outlook.com>
2025-05-12 17:53:47 +0200

    I´ve added some componentes created on the Home View

--- MyWeather/MyWeather/Views/Screens/HomeView.swift
+++ MyWeather/MyWeather/Views/Screens/HomeView.swift
@@ -10,21 +10,36 @@ import SwiftUI
 struct HomeView: View {
     @EnvironmentObject private var viewModel: WeatherViewModel
     
-    // TODO: Design the view
     var body: some View {
-        VStack(spacing: 20) {
-            ForEach(self.viewModel.nearestArea) { area in
-                if let locality = area.region.first?.value {
-                    Text(locality)
-                        .font(.title)
-                        .fontWeight(.bold)
+        ScrollView {
+            VStack {
+                HStack {
+                    Image(systemName: "paperplane.fill")
+                    Text("HOME")
+                        .font(.callout)
                 }
+                .shadow(radius: 10)
+                .padding(.top, 45)
+
+                ForEach(self.viewModel.nearestArea) { area in
+                    if let locality = area.areaName.first?.value {
+                        Text(locality)
+                            .font(.system(size: 38))
+                            .padding(.top, 10)
+                    }
+                }
+
+                ForEach(self.viewModel.currentCondition) { currentCondition in
+                    CurrentConditionView(currentCondition: currentCondition)
+                }
+
+                Spacer()
             }
         }
+        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        .foregroundStyle(.white)
+        .shadow(radius: 10)
+        .background(Color(.cyan))
         .padding()
     }
-}
-
-#Preview {
-    HomeView()
 }
